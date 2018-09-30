cd4dc7870e2fff15379eb207ed6b13ccfcd21d77
wangyazhe <wangyazhe@antiy.cn>
2018-09-30 16:27:23 +0800

    again update today

---
+++ src/core/spark_job.py
@@ -0,0 +1,4 @@
+class SparkJobManager:
+
+    def __init__(self):
+        pass
--- src/core/spark_runtime.py
+++ src/core/spark_runtime.py
@@ -1,6 +1,47 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+
 class SparkRuntime:
-    def __init__(self):
-        pass
+    __instance = None
+
+    def __init__(self, _params):
+        self.params = _params
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
+    def is_local_master(self):
+        master = self.params.get("xmatrix.master", "")
+        return master == "local" or master.startwith("local[")
 
     def create_runtime(self):
+        conf = SparkConf()
+        for k in self.params.keys():
+            if k.startwith("spark.") or k.startwith("hive."):
+                conf.set(k, self.params[k])
+
+        if "xmatrix.master" in self.params:
+            conf.setMaster(self.params["xmatrix.master"])
+
+        conf.setAppName(self.params["xmatrix.name"])
+
+        sparkSession = SparkSession.builder.config(conf)
+
+        if bool(self.params.get("xmatrix.enableHiveSupport", "false")):
+            sparkSession.enableHiveSupport()
+
+        ss = sparkSession.getOrCreate()
+
+        job_type = self.params.get("xmatrix.type", "script")
+
+        if job_type == "stream" and "xmatrix.metrics.kafka" in self.params:
+            ss.sparkContext.setLocalProperty(
+                "kafkaAddr", self.params["xmatrix.metrics.kafka"])
+
+        return ss
+
+    def register_udf(self, clzz):
         pass
